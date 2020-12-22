b56c8eaac07fae9da950068a57261cbbd6f41d35
kicok <kicoker@gmail.com>
2020-12-22 17:57:45 +0900

    add Styling

--- src/component/App.js
+++ src/component/App.js
@@ -14,7 +14,10 @@ class App extends React.Component {
     });
 
     console.log(response);
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -23,13 +26,21 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
