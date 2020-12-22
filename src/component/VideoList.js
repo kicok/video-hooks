d955579cd68c48545358f5a390a54eeb2e45a6b3
kicok <kicoker@gmail.com>
2020-12-22 15:22:06 +0900

    add VideoItem in VideoList

--- src/component/VideoList.js
+++ src/component/VideoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
-  return <div>{videos.length}</div>;
+  const renderedList = videos.map((video) => {
+    return <VideoItem key={video.etag} video={video} />;
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
